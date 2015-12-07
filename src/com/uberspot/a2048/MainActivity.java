e2329a9c7e6213cc18633956a91d6e0edcc2978f
zionchan <zion_chan@dlsu.edu.ph>
2015-12-07 21:21:49 +0800

    yay

---
+++ src/com/uberspot/a2048/MainActivity.java
@@ -0,0 +1,161 @@
+
+package com.uberspot.a2048;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.webkit.WebSettings;
+import android.webkit.WebSettings.RenderPriority;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+    private long mLastBackPress;
+    private static final long mBackPressThreshold = 3500;
+    private static final String IS_FULLSCREEN_PREF = "is_fullscreen_pref";
+    private static boolean DEF_FULLSCREEN = true;
+    private long mLastTouch;
+    private static final long mTouchThreshold = 2000;
+    private Toast pressBackToast;
+
+    @SuppressLint({ "SetJavaScriptEnabled", "NewApi", "ShowToast" })
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        if (Build.VERSION.SDK_INT >= 11) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                    WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+        }
+
+
+        applyFullScreen(isFullScreen());
+
+
+        boolean isOrientationEnabled = false;
+        try {
+            isOrientationEnabled = Settings.System.getInt(getContentResolver(),
+                    Settings.System.ACCELEROMETER_ROTATION) == 1;
+        } catch (SettingNotFoundException e) { }
+
+
+        int screenLayout = getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK;
+        if (((screenLayout == Configuration.SCREENLAYOUT_SIZE_LARGE)
+                || (screenLayout == Configuration.SCREENLAYOUT_SIZE_XLARGE))
+                    && isOrientationEnabled) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+        }
+
+        setContentView(R.layout.activity_main);
+
+
+        mWebView = (WebView) findViewById(R.id.mainWebView);
+        WebSettings settings = mWebView.getSettings();
+        String packageName = getPackageName();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setRenderPriority(RenderPriority.HIGH);
+        settings.setDatabasePath("/data/data/" + packageName + "/databases");
+
+
+        if (savedInstanceState != null) {
+            mWebView.restoreState(savedInstanceState);
+        } else {
+            mWebView.loadUrl("file:///android_asset/2048/index.html");
+        }
+
+        Toast.makeText(getApplication(), R.string.toggle_fullscreen, Toast.LENGTH_SHORT).show();
+
+        mWebView.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                long currentTime = System.currentTimeMillis();
+                if ((event.getAction() == MotionEvent.ACTION_UP)
+                        && (Math.abs(currentTime - mLastTouch) > mTouchThreshold)) {
+                    boolean toggledFullScreen = !isFullScreen();
+                    saveFullScreen(toggledFullScreen);
+                    applyFullScreen(toggledFullScreen);
+                } else if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mLastTouch = currentTime;
+                }
+
+                return false;
+            }
+        });
+
+        pressBackToast = Toast.makeText(getApplicationContext(), R.string.press_back_again_to_exit,
+                Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        mWebView.saveState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return true;
+    }
+
+    private void saveFullScreen(boolean isFullScreen) {
+
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.putBoolean(IS_FULLSCREEN_PREF, isFullScreen);
+        editor.commit();
+    }
+
+    private boolean isFullScreen() {
+        return PreferenceManager.getDefaultSharedPreferences(this).getBoolean(IS_FULLSCREEN_PREF,
+                DEF_FULLSCREEN);
+    }
+
+
+    private void applyFullScreen(boolean isFullScreen) {
+        if (isFullScreen) {
+            getWindow().clearFlags(LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onBackPressed() {
+        long currentTime = System.currentTimeMillis();
+        if (Math.abs(currentTime - mLastBackPress) > mBackPressThreshold) {
+            pressBackToast.show();
+            mLastBackPress = currentTime;
+        } else {
+            pressBackToast.cancel();
+            super.onBackPressed();
+        }
+    }
+}
